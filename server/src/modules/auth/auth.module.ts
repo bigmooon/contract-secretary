6b79a8de197e23ca288dc9041cde1c3af4896553
jpaper <bigmooon1215@gmail.com>
2025-12-18 23:24:55 +0900

    feat(auth): configure AuthModule with global JWT guard

---
+++ server/src/modules/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: (): JwtModuleOptions => {
+        const expiresIn = process.env.JWT_ACCESS_EXPIRES_IN || '15m';
+        const jwtSecret = process.env.JWT_SECRET;
+        if (!jwtSecret) {
+          throw new Error('[JWT Module] JWT_SECRET is not set');
+        }
+
+        return {
+          secret: jwtSecret,
+          signOptions: {
+            expiresIn: expiresIn as unknown as number,
+          },
+        };
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
