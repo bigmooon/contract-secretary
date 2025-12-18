4bf1ff0d7cfea9767fc4c1298acce76911af5062
jpaper <bigmooon1215@gmail.com>
2025-12-19 00:34:51 +0900

    feat(server): configure global ValidationPipe for request validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('계약비서 API')
